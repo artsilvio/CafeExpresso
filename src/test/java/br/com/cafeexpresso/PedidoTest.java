30af7ce46bafd4ee78d7db75080be1201300dde3
Marcos <marcoswestwood@gmail.com>
2026-05-07 18:34:57 -0300

    Implementação de teste para calcular valor total do pedido

---
+++ src/test/java/br/com/cafeexpresso/PedidoTest.java
@@ -0,0 +1,17 @@
+package br.com.cafeexpresso;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PedidoTest {
+    
+    @Test
+    void deveCalcularTotalComSucesso() {
+        Pedido pedido = new Pedido();
+        Produto cafe = new Produto("Café", 15.0);
+
+        pedido.adicionarItem(new ItemPedido(cafe, 2));
+
+        assertEquals(30.0, pedido.calcularTotal());
+    }
+}
